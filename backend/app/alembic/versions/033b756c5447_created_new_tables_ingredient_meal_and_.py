ed37c2b24694fee92642c1c58939f5ee8f27e44d
sc19sjjd <sc19sjjd@leeds.ac.uk>
2023-07-10 16:00:42 +0100

    migrate database for new tables

---
+++ backend/app/alembic/versions/033b756c5447_created_new_tables_ingredient_meal_and_.py
@@ -0,0 +1,24 @@
+"""created new tables ingredient, meal and diet requirements
+
+Revision ID: 033b756c5447
+Revises: 91979b40eb38
+Create Date: 2023-07-10 07:52:55.987857-07:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '033b756c5447'
+down_revision = '91979b40eb38'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
