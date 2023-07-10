21210525f20f937da6e030802f2af8b94c0afc30
sc19sjjd <sc19sjjd@leeds.ac.uk>
2023-07-10 15:48:08 +0100

    added schemas for new models, slight user model changes

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -17,11 +17,11 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
-    first_name = Column(String)
-    last_name = Column(String)
+    name = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     is_superuser = Column(Boolean, default=False)
+    is_verified = Column(Boolean, default=False)
 
 
 class UserDietRequirements(Base):
--- backend/app/db/schemas.py
+++ backend/app/db/schemas.py
@@ -7,8 +7,8 @@ class UserBase(BaseModel):
     email: str
     is_active: bool = True
     is_superuser: bool = False
-    first_name: str = None
-    last_name: str = None
+    is_verified: bool = False
+    name: str = None
 
 
 class UserOut(UserBase):
@@ -23,6 +23,8 @@ class UserCreate(UserBase):
 
 
 class UserEdit(UserBase):
+    email: t.Optional[str] = None
+    name: t.Optional[str] = None
     password: t.Optional[str] = None
 
     class Config:
@@ -44,3 +46,109 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     email: str = None
     permissions: str = "user"
+
+
+class UserDietRequirementsBase(BaseModel):
+    is_vegetarian: bool = False
+    is_vegan: bool = False
+    is_gluten_free: bool = False
+    is_dairy_free: bool = False
+    is_nut_free: bool = False
+    is_shellfish_free: bool = False
+    is_pescatarian: bool = False
+
+
+class UserDietRequirementsOut(UserDietRequirementsBase):
+    pass
+
+
+class UserDietRequirementsCreate(UserDietRequirementsBase):
+    class Config:
+        orm_mode = True
+
+
+class UserDietRequirementsEdit(UserDietRequirementsBase):
+    is_vegetarian: t.Optional[bool] = None
+    is_vegan: t.Optional[bool] = None
+    is_gluten_free: t.Optional[bool] = None
+    is_dairy_free: t.Optional[bool] = None
+    is_nut_free: t.Optional[bool] = None
+    is_shellfish_free: t.Optional[bool] = None
+    is_pescatarian: t.Optional[bool] = None
+
+    class Config:
+        orm_mode = True
+
+
+class UserDietRequirements(UserDietRequirementsBase):
+    id: int
+    user_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class IngredientBase(BaseModel):
+    name: str
+    alias: str = None
+
+
+class IngredientOut(IngredientBase):
+    pass
+
+
+class IngredientCreate(IngredientBase):
+    class Config:
+        orm_mode = True
+
+
+class IngredientEdit(IngredientBase):
+    name: t.Optional[str] = None
+    alias: t.Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+
+class Ingredient(IngredientBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class MealBase(BaseModel):
+    name: str
+    description: str = None
+
+
+class MealOut(MealBase):
+    ingredients: t.List[IngredientOut] = []
+
+    class Config:
+        orm_mode = True
+
+
+class MealCreate(MealBase):
+    ingredients: t.List[int] = []
+
+    class Config:
+        orm_mode = True
+
+
+class MealEdit(MealBase):
+    name: t.Optional[str] = None
+    description: t.Optional[str] = None
+    ingredients: t.Optional[t.List[int]] = None
+
+    class Config:
+        orm_mode = True
+
+
+class Meal(MealBase):
+    id: int
+    user_id: int
+    ingredients: t.List[Ingredient] = []
+
+    class Config:
+        orm_mode = True
