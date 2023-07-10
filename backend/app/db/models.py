6c33784d219cff227e41b10f0cef985340172526
sc19sjjd <sc19sjjd@leeds.ac.uk>
2023-07-10 15:29:02 +0100

    added meal, ingredient and diet requirement models

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,6 +1,15 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import relationship
 
 from .session import Base
+
+MealIngredient = Table(
+    "meal_ingredient",
+    Base.metadata,
+    Column("id", Integer, primary_key=True, index=True),
+    Column("meal_id", Integer, ForeignKey("meal.id")),
+    Column("ingredient_id", Integer, ForeignKey("ingredient.id")),
+)
 
 
 class User(Base):
@@ -13,3 +22,40 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     is_superuser = Column(Boolean, default=False)
+
+
+class UserDietRequirements(Base):
+    __tablename__ = "user_diet_requirements"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    is_vegetarian = Column(Boolean, default=False)
+    is_vegan = Column(Boolean, default=False)
+    is_gluten_free = Column(Boolean, default=False)
+    is_dairy_free = Column(Boolean, default=False)
+    is_nut_free = Column(Boolean, default=False)
+    is_shellfish_free = Column(Boolean, default=False)
+    is_pescatarian = Column(Boolean, default=False)
+
+
+class Ingredient(Base):
+    __tablename__ = "ingredient"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    alias = Column(String)
+
+
+class Meal(Base):
+    __tablename__ = "meal"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    user_id = Column(Integer, ForeignKey("user.id"))
+
+    ingredients = relationship(
+        "Ingredient",
+        secondary=MealIngredient,
+        back_populates="meals",
+    )
