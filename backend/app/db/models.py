91aa4dde6b5591e84349e3f6f8d6c9c9a81500ab
sc19sjjd <sc19sjjd@leeds.ac.uk>
2023-07-10 16:52:44 +0100

    user model name no longer required field

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -17,7 +17,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
-    name = Column(String, nullable=False)
+    name = Column(String)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     is_superuser = Column(Boolean, default=False)
